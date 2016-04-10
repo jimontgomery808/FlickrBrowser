f4477eb6dd81133d5928762266e666a377c3fa79
jimontgomery808 <jimontgomery808@gmail.com>
2016-04-09 18:31:26 -0700

    Imported new theme

--- app/src/main/java/com/example/josh/flickrbrowser/FlickrImageViewHolder.java
+++ app/src/main/java/com/example/josh/flickrbrowser/FlickrImageViewHolder.java
@@ -8,11 +8,13 @@ import android.widget.TextView;
 /**
  * Created by dev on 17/02/2016.
  */
-public class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+public class FlickrImageViewHolder extends RecyclerView.ViewHolder
+{
     protected ImageView thumbnail;
     protected TextView title;
 
-    public FlickrImageViewHolder(View view) {
+    public FlickrImageViewHolder(View view)
+    {
         super(view);
         this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
         this.title = (TextView) view.findViewById(R.id.title);
